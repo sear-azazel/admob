2cea6b3c9c9c992b4addf8fbc3bed2df741fa45a
sear-azazel <sear.azazel@gmail.com>
2019-05-13 00:27:48 +0900

    feat: 広告ID更新

--- Assets/Azuma/Scripts/Ids/AdMobIds.cs
+++ Assets/Azuma/Scripts/Ids/AdMobIds.cs
@@ -8,21 +8,21 @@
         /// <summary>
         /// The android banner.
         /// </summary>
-        public static readonly string Android_Banner = "";
+        public static readonly string Android_Banner = "ca-app-pub-1250662536437655/7251791252";
 
         /// <summary>
         /// The android interstitial.
         /// </summary>
-        public static readonly string Android_Interstitial = "";
+        public static readonly string Android_Interstitial = "ca-app-pub-1250662536437655/4942512050";
 
         /// <summary>
         /// The ios banner.
         /// </summary>
-        public static readonly string ios_Banner = "";
+        public static readonly string ios_Banner = "ca-app-pub-1250662536437655/3653481113";
 
         /// <summary>
         /// The ios interstitial.
         /// </summary>
-        public static readonly string ios_Interstitial = "";
+        public static readonly string ios_Interstitial = "ca-app-pub-1250662536437655/9835746080";
     }
 }
